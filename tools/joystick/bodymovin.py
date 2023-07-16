4c1b9c28146dda24c946fb982c628299a0da9f95
Comma Device <device@comma.ai>
2023-07-16 02:00:08 -0700

    follow func in bodymovin

--- tools/joystick/bodymovin.py
+++ tools/joystick/bodymovin.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 import cereal.messaging as messaging
 from cereal import log
@@ -7,7 +8,9 @@ from cereal.visionipc import VisionIpcClient, VisionStreamType
 IMAGE_HEIGHT, IMAGE_WIDTH = 416, 640
 POS_EPS = 0.1
 PERSON_WIDTH_TARGET = 0.4
-
+PERSON_HEIGHT_TARGET = 0.55
+IDLE_CNT_TARGET = 20
+YOLO_MIN_CONF = 0.7
 
 def get_control_command(x, y):
     x = max(-1.0, min(1.0, x))
@@ -19,27 +22,13 @@ def get_control_command(x, y):
 
     return dat
 
-def asleep_face_msg():
-    # TODO doesn't work, return None for now
-    return None
-    # dat = messaging.new_message('bodyFace')
-    # dat.bodyFace = log.BodyFace.sleep
-    # return dat 
-
-def awake_face_msg():
-    # TODO doesn't work, return None for now
-    return None
-    # dat = messaging.new_message('bodyFace')
-    # dat.bodyFace = log.BodyFace.awake
-    # return dat 
-
-{'label': 'person', 'location': {'height': 44.655364990234375, 'width': 21.4017333984375, 'x': 279.86962890625, 'y': 81.7782211303711}, 'score': 0.7005926966667175}
-
-def run_loop():
-    pm = messaging.PubMaster(['testJoystick', 'bodyFace'])
+def find_and_follow_human():
+    pm = messaging.PubMaster(['testJoystick'])#, 'bodyFace'])
     sm = messaging.SubMaster(['logMessage'])
-        
-    while True:
+
+    follow_active = True
+    idle_cnt = 0
+    while follow_active:
         sm.update(50)
         if not sm.updated["logMessage"]:
             print("No msgs")
@@ -50,12 +39,12 @@ def run_loop():
         if code != 0:
             continue
 
-        cmd_msg, face_msg = None, None
-        assert type(log_msg) == list
-        person_msg = next((msg for msg in data if msg["label"] == "person"), None)
-        if person_msg == None:
-            face_msg = asleep_face_msg()
-        else:
+        cmd_msg = None
+        people_msgs = [msg for msg in data if msg["label"] == "person" and msg["score"] > YOLO_MIN_CONF]
+        if len(people_msgs) != 0:
+            person_msg = max(people_msgs, key=lambda a: a["location"]["height"])
+            # TODO take person closest to center 
+            print("found a person")
             pt1 = person_msg["location"]["x"], person_msg["location"]["y"]
             pt2 = person_msg["location"]["x"] + person_msg["location"]["width"], person_msg["location"]["y"] + person_msg["location"]["height"]
             pt1 = (pt1[0] / IMAGE_WIDTH, pt1[1] / IMAGE_HEIGHT)
@@ -64,20 +53,34 @@ def run_loop():
 
             center_x = (pt1[0] + pt2[0]) / 2
             rect_width = abs(pt1[0] - pt2[0])
-            face_msg = awake_face_msg()
+            rect_height = abs(pt1[1] - pt2[1])
+            print("width:", rect_width, "height:", rect_height)
+            is_idling = False
             if 0.5 - POS_EPS < center_x < 0.5 + POS_EPS:
-                if pt1[1] > POS_EPS and pt2[1] < 1 - POS_EPS and rect_width > PERSON_WIDTH_TARGET:
+                if rect_height < PERSON_HEIGHT_TARGET:
                     cmd_msg = get_control_command(-1, 0)
+                else:
+                    is_idling = True
+                    cmd_msg = get_control_command(0, 0)
             elif 0.5 - POS_EPS > center_x:
                 cmd_msg = get_control_command(0, 1)
             else:
                 cmd_msg = get_control_command(0, -1)
-            print("Messages:", cmd_msg is None, face_msg is None)
+            if is_idling:
+                idle_cnt += 1
+            else:
+                idle_cnt = 0
+            print("Messages:", cmd_msg is None)
 
+        print("cmd_msg:", cmd_msg is not None)
         if cmd_msg is not None:
+            print("axes:", cmd_msg.testJoystick.axes)
             pm.send('testJoystick', cmd_msg)
-        if face_msg is not None:
-            pm.send('bodyFace', face_msg)
+
+        if idle_cnt > IDLE_CNT_TARGET:
+            follow_active = False
+
+        time.sleep(0.050)
 
 if __name__=="__main__":
-    run_loop()
+    find_and_follow_human()
